a235135e5829ea4b53be085c4d0bdca055f55259
SelinaZJW <selina.zheng@sky.uk>
2025-04-06 19:53:26 +0100

    experiment closing websocket frame

--- app-tapir/core/src/main/scala/com/selinazjw/rtqs/routes/Routes.scala
+++ app-tapir/core/src/main/scala/com/selinazjw/rtqs/routes/Routes.scala
@@ -23,29 +23,36 @@ import sttp.tapir.server.http4s.Http4sServerInterpreter
 final class Routes[F[_] : Async](userService: UserService[F], workerService: WorkerService[F]) {
 
   private def endpointToServerWebsocket(
-      userSessionId: String): Either[(StatusCode, ErrorResponse), Pipe[F, String, PositionUpdate]] =
+      userSessionId: String): Either[(StatusCode, ErrorResponse), Pipe[F, String, Option[PositionUpdate]]] =
     userSessionId match {
       case "" =>
         (StatusCode.BadRequest, ErrorResponse(400, "User session ID cannot be empty"))
-          .asLeft[Pipe[F, String, PositionUpdate]]
+          .asLeft[Pipe[F, String, Option[PositionUpdate]]]
       case _ =>
-        ((_: Stream[F, String]) => userService.addUserAndSubscribe(UserSessionId(userSessionId)))
+        ((_: Stream[F, String]) =>
+          userService.addUserAndSubscribe(UserSessionId(userSessionId)).map {
+            case PositionUpdate(0) => None    // emitting None does not closer frame for websocat
+            case positionUpdate    => positionUpdate.some
+          })
           .asRight[(StatusCode, ErrorResponse)]
     }
 
   val userServiceEndpoint: Endpoint[Unit,
                                     String,
                                     (StatusCode, ErrorResponse),
-                                    Pipe[F, String, PositionUpdate],
-                                    Fs2Streams[F] & capabilities.WebSockets] = endpoint.get   // websocket generally uses get
-    .in("add-user-and-subscribe")
-    .in(query[String]("userSessionId"))    // unable to send UserSessionId as request jsonBody, send userSessionId as query param
-    .out(
-      webSocketBody[String, CodecFormat.TextPlain, PositionUpdate, CodecFormat.Json](Fs2Streams[F])
-    ) // stream entire messages ignoring input stream
-    // .out(streamBody(Fs2Streams[F])(Schema.derived[PositionUpdate], CodecFormat.Json()))
-    // only allow stream of bytes, also can't signal end of stream
-    .errorOut(statusCode and jsonBody[ErrorResponse])
+                                    Pipe[F, String, Option[PositionUpdate]],
+                                    Fs2Streams[F] & capabilities.WebSockets] =
+    endpoint.get // websocket generally uses get
+      .in("add-user-and-subscribe")
+      .in(
+        query[String]("userSessionId")
+      ) // unable to send UserSessionId as request jsonBody, send userSessionId as query param
+      .out(
+        webSocketBody[String, CodecFormat.TextPlain, Option[PositionUpdate], CodecFormat.Json](Fs2Streams[F]).decodeCloseResponses(true)
+      ) // stream entire messages ignoring input stream
+      // .out(streamBody(Fs2Streams[F])(Schema.derived[PositionUpdate], CodecFormat.Json()))
+      // only allow stream of bytes, also can't signal end of stream
+      .errorOut(statusCode and jsonBody[ErrorResponse])
 
   val userServiceServerEndpoint: ServerEndpoint[Fs2Streams[F] & capabilities.WebSockets, F] =
     userServiceEndpoint.serverLogicPure(useSessionId => endpointToServerWebsocket(useSessionId))
